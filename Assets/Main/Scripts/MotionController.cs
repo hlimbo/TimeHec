5518129bb52b2be2bb18e357f86f2ce9e4265485
hlimbo <hlimbo@users.noreply.github.com>
2017-11-09 19:23:19 -0800

    Add 3rd type of motion control for player vessel

--- Assets/Main/Scripts/MotionController.cs
+++ Assets/Main/Scripts/MotionController.cs
@@ -24,47 +24,95 @@ public class MotionController : MonoBehaviour {
     public float motionSpeed;
     public Vector3 moveVelocity;
 
+    Vector3 oldRawMousePos;
+
     void Start()
     {
+        oldRawMousePos = Vector3.zero;
         motionSpeed = 0.0f;
         moveVelocity = Vector3.zero;
 
         oldMousePos = Vector3.zero;
         newMousePos = Vector3.zero;
-        movePercent = Input.touchSupported ? 0.01f : 0.5f;
+        // movePercent = Input.touchSupported ? 0.01f : 0.5f;
+        movePercent = 0.5f;
     }
 
 	void Update ()
     {
-        if(Input.touchSupported)
+        Movement3();
+    }
+
+    //move based on the direction of mouse swipe
+    void MouseMovement1()
+    {
+        newMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //left click hold
+        if (Input.GetMouseButton(0))
         {
-            if(Input.touchCount == 1)
+            //move ship towards finger position (add some offset here...)
+            deltaMousePos = (newMousePos - oldMousePos);
+            if (deltaMousePos.sqrMagnitude > tolerance)
             {
-                switch(Input.GetTouch(0).phase)
-                {
-                    case TouchPhase.Moved:
-                        deltaMousePos = Input.GetTouch(0).deltaPosition;
-                        motionSpeed = deltaMousePos.magnitude / Input.GetTouch(0).deltaTime;
-                        moveVelocity = motionSpeed * deltaMousePos.normalized * Time.deltaTime * movePercent;
-                        transform.position += moveVelocity;
-                        break;
-                }
+                transform.position = Vector3.Lerp(transform.position, transform.position + deltaMousePos, movePercent);
             }
         }
-        else
+        oldMousePos.Set(newMousePos.x, newMousePos.y, newMousePos.z);
+    }
+
+    //move based on the direction of finger swipe
+    void TouchMovement1()
+    {
+        if (Input.touchCount == 1)
         {
-            newMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //left click hold
-            if (Input.GetMouseButton(0))
+            switch (Input.GetTouch(0).phase)
             {
-                //apply vessel movement
-                deltaMousePos = (newMousePos - oldMousePos);
-                if (deltaMousePos.sqrMagnitude > tolerance)
-                {
-                    transform.position = Vector3.Lerp(transform.position, transform.position + deltaMousePos, movePercent);
-                }
+                //movePercent needs to be 0.01 here
+                case TouchPhase.Moved:
+                    //move ship in the direction of finger swipe
+                    deltaMousePos = Input.GetTouch(0).deltaPosition;
+                    motionSpeed = deltaMousePos.magnitude / Input.GetTouch(0).deltaTime;
+                    moveVelocity = motionSpeed * deltaMousePos.normalized * Time.deltaTime * movePercent;
+                    transform.position += moveVelocity;
+                    break;
             }
-            oldMousePos.Set(newMousePos.x, newMousePos.y, newMousePos.z);
+        }
+    }
+
+    float Hermite(float v)
+    {
+        return v * v * (3.0f - 2.0f * v);
+    }
+
+    Transform oldTransform;
+    void Movement3()
+    {
+        //smoothly move towards on touch began
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("mouse pressed");
+            oldTransform = transform;
+            //Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Vector3 lockPosition = oldTransform.TransformPoint(Vector3.down);// 1 unity unit below triangle
+            ////The further away mouse press location is from vessel, the slower the vessel moves to mouse press location
+            //float p = Mathf.InverseLerp(0.0f, 1.0f, lockPosition.magnitude / (targetPos - lockPosition).magnitude);
+            //lockPosition = Vector3.MoveTowards(lockPosition, targetPos, (targetPos - lockPosition).magnitude);
+            ////lockPosition = Vector3.Lerp(lockPosition, targetPos, p);
+            //transform.position = lockPosition + Vector3.up;
+            //oldRawMousePos = Input.mousePosition;
+        }
+
+        //follow where mouse cursor points towards with some
+        //offset so the vessel isn't directly on top of mouse cursor / finger
+        else if ((Input.mousePosition - oldRawMousePos).magnitude > tolerance && Input.GetMouseButton(0))
+        {
+            Debug.Log("mouse held");
+            Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 lockPosition = oldTransform.TransformPoint(Vector3.down);// 1 unity unit below triangle
+            lockPosition = Vector3.Lerp(lockPosition, targetPos, movePercent);
+            lockPosition.z = 0.0f;
+            transform.position = lockPosition + Vector3.up;
+            oldRawMousePos = Input.mousePosition;
         }
     }
 
