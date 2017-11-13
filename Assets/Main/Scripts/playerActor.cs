5f4ba8a3d200bf68753b165c63d053249b3f8e29
hlimbo <hlimbo@users.noreply.github.com>
2017-11-12 20:32:28 -0800

    correct rotations to match the direction right joystick is pointing

--- Assets/Main/Scripts/playerActor.cs
+++ Assets/Main/Scripts/playerActor.cs
@@ -5,35 +5,25 @@ public class playerActor : MonoBehaviour {
 
     public TouchJoystickController leftJoystick;
     public TouchJoystickController rightJoystick;
-    public GameObject shieldObject;
+    public Transform shieldPivot;
     public Text positionText;
     public float moveSpeed;
-
     //2^-7
     private float tolerance = 0.0078125f;
+    public float angularSpeed = 90.0f;
+    public float rotationRadius = 1.5f;
 
-    private float tolerance2 = 0.00001f;
-
-    void Start () {
-  
-    }
-	
 	// Update is called once per frame
 	void Update () {
 
+        //have the analog sticks point to the direction the shield should be facing!
+        //I needed to swap the x and y components for this to work!
+        float targetAngleInRadians = Mathf.Atan2(-1.0f * rightJoystick.inputDirection.x, rightJoystick.inputDirection.y);
+        Quaternion targetRotation = Quaternion.Euler(0.0f, 0.0f, targetAngleInRadians * Mathf.Rad2Deg);
+        shieldPivot.rotation = Quaternion.RotateTowards(shieldPivot.rotation, targetRotation, angularSpeed * Time.deltaTime);
+
         transform.Translate(leftJoystick.inputDirection * moveSpeed * Time.deltaTime);
         positionText.text = transform.position.ToString();
-
-        //position the shield relative to where the player vessel is located
-        //half circle rotation
-        //if cos and sin values are close enough to being zero... set them as zero values.
-        float x = Mathf.Abs(Mathf.Cos(Mathf.PI / 2.0f + (rightJoystick.inputDirection.x * Mathf.PI / 2.0f))) > tolerance ? -1.5f * Mathf.Cos(Mathf.PI / 2.0f + (rightJoystick.inputDirection.x * Mathf.PI / 2.0f)) : 0.0f;
-        float y = Mathf.Abs(Mathf.Sin(Mathf.PI / 2.0f + (rightJoystick.inputDirection.x * Mathf.PI))) > tolerance ? 1.5f * Mathf.Sin(Mathf.PI / 2.0f + (rightJoystick.inputDirection.x * Mathf.PI / 2.0f)) : 0.0f;
-        Vector2 shieldPos = new Vector2(x,y);
-        shieldObject.transform.position = transform.TransformPoint(shieldPos);
-        float targetAngleInRadians = Mathf.Atan2(y, x) - Mathf.PI / 2.0f;
-        Quaternion newQuat = Quaternion.Euler(0.0f, 0.0f, targetAngleInRadians * Mathf.Rad2Deg);
-        shieldObject.transform.rotation = Quaternion.RotateTowards(shieldObject.transform.rotation, newQuat, 90.0f * Time.deltaTime);
-        //shieldObject.transform.RotateAround(transform.position, Vector3.forward, 45.0f * Time.deltaTime);
+        shieldPivot.transform.position = this.transform.position;
     }
 }
